9cba8d72ef2077d3a4d22fc8f0d1203b395b811c
suhaibh27 <40864369+suhaibh27@users.noreply.github.com>
2021-04-25 17:01:12 +0300

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-//mango
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+//mango potatoe
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
